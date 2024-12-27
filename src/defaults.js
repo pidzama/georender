05ee1dcfc36c741d7daac0631515396bac73d6d6
pidzama <pidzama@gmail.com>
2024-12-27 20:18:13 +0100

    Changes for rendering images in subdirectories

--- src/defaults.js
+++ src/defaults.js
@@ -3,10 +3,11 @@ import { Style, Icon, Circle, Fill, Stroke } from 'ol/style.js'
 
 const white   = 'rgba(255, 255, 255, 0.85)';
 const blue    = '#1267FF';
+const red     = '#C70039';
 
 export const defaults = {
-  width: 500,
-  height: 500,
+  width: 1920,
+  height: 1080,
   tile: {
     osm: new OSM({
       url: 'https://cdn.devgrid.net/osm/{z}/{x}/{y}.png',
@@ -20,45 +21,53 @@ export const defaults = {
     esri: new XYZ({
       url: 'https://server.arcgisonline.com//ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
       maxZoom: 23,
-      crossOrigin: 'anonymous' 
+      crossOrigin: 'anonymous'
     })
   },
   style: [
     new Style({
       stroke: new Stroke({
-        color: white,
-        width: 6,
-      }),
-      image: new Circle({
-        radius: 6,
-        fill: new Fill({
-          color: white
-        })
-      })
-    }),
-    new Style({
-      stroke: new Stroke({
-        color: blue,
-        width: 5
-      }),
-      image: new Circle({
-        radius: 4,
-        stroke: new Stroke({
-          color: blue,
-          width: 2
-        })
-      })
-    }),
-    new Style({
-      image: new Icon({
-        scale: 0.5,
-        rotateWithView: false,
-        anchor: [0.5, 1],
-        opacity: 1,
-        src: 'https://app.gpxlab.net/assets/pin.png'
-      })
-    })  
+        color: red,
+        width: 2,
+      })//,
+      // image: new Circle({
+      //   radius: 6,
+      //   fill: new Fill({
+      //     color: white
+      //   })
+      // })
+    })
+    // ,
+    // new Style({
+    //   stroke: new Stroke({
+    //     color: red,
+    //     width: 2
+    //   })//,
+    //   // image: new Circle({
+    //   //   radius:1,
+    //   //   stroke: new Stroke({
+    //   //     color: red,
+    //   //     width: 1
+    //   //   })
+    //   // })
+    // }),
+    // new Style({
+    //   stroke: new Stroke({
+    //     color: red,
+    //     width: 2
+    //   })
+    // })
+    // // new Style({
+    // //   image: new Icon({
+    // //     scale: 0.5,
+    // //     rotateWithView: false,
+    // //     anchor: [0.5, 1],
+    // //     opacity: 1,
+    // //     src: 'https://app.gpxlab.net/assets/pin.png'
+    // //   })
+    // //
+    // // })
   ],
   padding: [50, 50, 50, 50],
-  timeout: 30
+  timeout: 300
 };
--- src/renderer.js
+++ src/renderer.js
@@ -19,56 +19,68 @@ export class Renderer {
         const tile = defaults.tile[options.tile || 'osm'];
         const style = defaults.style;
         const padding = defaults.padding;
-    
+
         console.log('  width:', width);
         console.log('  height:', height);
         console.log('  input files:', inputFiles);
         console.log('  output file:', outputFile);
-    
+
         const timeout = setTimeout(reject, defaults.timeout * 1000);
-  
+
         const element = document.createElement('div');
         element.style.width = `${width}px`;
         element.style.height = `${height}px`;
         document.body.appendChild(element);
-    
+
         const map = new Map({ target: element });
         map.addLayer(new TileLayer({ source: tile }));
-    
+
         const featureSource = new VectorSource();
         const featureLayer = new VectorLayer({
           source: featureSource,
           style
         });
         map.addLayer(featureLayer);
-  
+
         const view = new View();
-        map.setView(view); 
-        
+        map.setView(view);
+
         const importer = new Importer();
-        for (let i = 0; i < inputFiles.length; i++) {
-          featureSource.addFeatures(await importer.importFile(inputFiles[i]));
+
+        const files = [];
+        const dirname = inputFiles[0];
+        const imageFile = dirname + "map.jpg";
+
+        fs.readdirSync(dirname).forEach(file => {
+
+            if(file.endsWith(".kml")){
+              files.push(dirname +file);
+            }
+
+        });
+        for (let i = 0; i < files.length; i++) {
+          featureSource.addFeatures(await importer.importFile(files[i]));
         }
-  
+
         view.fit(featureSource.getExtent(), {
           size: map.getSize(),
           padding
         });
-  
+
         map.on('postrender', async () => process.stdout.write("."));
-  
+
         map.once('rendercomplete', async () => {
-          console.log("\nRender completed, saving image to", outputFile);
+          console.log("\nRender completed, saving image to", imageFile);
           clearTimeout(timeout);
-  
+
           const canvas = await this._processCanvas(map);
-          await this._saveImage(canvas, outputFile)
-  
+          await this._saveImage(canvas, imageFile)
+
           resolve();
         })
-  
+
         map.renderSync();
-  
+
       } catch (err) {
         reject(err);
       }
@@ -79,7 +91,7 @@ export class Renderer {
   }
 
   async _processCanvas(map) {
-    // OpenLayers can use multiple canvases however it seems in Node.js 
+    // OpenLayers can use multiple canvases however it seems in Node.js
     // only one Canvas object is being used for rendering.
     // @TODO: May be needed to fix this in future.
     const canvases = map.getViewport().querySelectorAll('.ol-layer canvas, canvas.ol-layer');
@@ -96,7 +108,7 @@ export class Renderer {
       throw new Error("Only PNG and JPEG output files are supported");
     return new Promise((resolve, reject) => {
       canvas.toBlob(
-        blob => { 
+        blob => {
           const reader = new FileReader();
           reader.onloadend = () => {
             fs.writeFileSync(filename, reader.result, 'binary');
